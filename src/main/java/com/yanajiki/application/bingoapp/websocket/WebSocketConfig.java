28d2f04c80ac44c1d5b476264a35283ab3dcc7ac
hi-im-yan <yanajiki2010@hotmail.com>
2026-03-26 21:57:18 -0400

    config: websocket emmiting heartbeat

--- src/main/java/com/yanajiki/application/bingoapp/websocket/WebSocketConfig.java
+++ src/main/java/com/yanajiki/application/bingoapp/websocket/WebSocketConfig.java
@@ -1,8 +1,11 @@
 package com.yanajiki.application.bingoapp.websocket;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -35,8 +38,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	 */
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/room", "/testing"); // topic prefix
+		config.enableSimpleBroker("/room", "/testing") // topic prefix
+				.setHeartbeatValue(new long[]{10000, 10000})
+				.setTaskScheduler(brokerTaskScheduler());
+
 		config.setApplicationDestinationPrefixes("/app"); // send message prefix
+	}
+
+	/**
+	 * Task scheduler required by the simple broker to send STOMP heartbeat frames.
+	 *
+	 * @return a single-threaded task scheduler for broker heartbeats
+	 */
+	@Bean
+	public TaskScheduler brokerTaskScheduler() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(1);
+		scheduler.setThreadNamePrefix("wss-heartbeat-");
+		return scheduler;
 	}
 
 	/**
